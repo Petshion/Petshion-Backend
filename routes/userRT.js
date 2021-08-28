f4856663352a862f37741b8ebdb8882eb337727a
MurasakiPurki <chajuhan620@gmail.com>
2021-08-28 14:43:05 +0900

    fix: redirect to /main

--- routes/userRT.js
+++ routes/userRT.js
@@ -3,7 +3,7 @@ const passport = require('../config/passport.js');
 module.exports = function(app, user){
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile','email','openid']}));
 
-    app.get('/auth/google/PetshionOauth', passport.authenticate('google'), (res, req)=>{
+    app.get('/auth/google/PetshionOauth', passport.authenticate('google'), (req, res)=>{
         res.redirect('/main');
     })
 }
